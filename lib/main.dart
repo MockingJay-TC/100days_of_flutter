946fed087b3e5a2b429310986d3b30e1dd0500bf
Victor-Aremu <77357316+Victor-Aremu@users.noreply.github.com>
2021-08-03 13:54:35 +0000

    added a button

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,17 @@ class MyApp extends StatefulWidget {
 
 // creating a state class extending state with parameter MyApp
 class _State extends State<MyApp> {
+  
+  String _value = 'Hello World!!';
+
+  void changeName() {
+    setState(() {
+      _value = "My name is Victor";
+    });
+  }
+
   @override
+  // widget build is like rendering, and this presents whatever widget to the screen
   Widget build(BuildContext context) {
     // returning a Scaffold to the screen
     // Scaffolds are of different layers
@@ -32,16 +42,25 @@ class _State extends State<MyApp> {
       ),
       // now to the second layer, which is the body, this where the main contenet is going to be.
       body: new Container(
+        // add padding to all sides of the body content
         padding: new EdgeInsets.all(32.0),
+        // center the content in the body
         child: new Center(
+          // passing content to a column
           child: new Column(
+            // column takes children which includes Text
             children: <Widget>[
-              new Text("Hello World!!!")
+              // passing a text to the body
+              new Text(_value),
+              new ElevatedButton(
+                  onPressed: () {
+                    changeName();
+                  },
+                  child: new Text("Click Me"))
             ],
           ),
         ),
       ),
     );
-
   }
 }
